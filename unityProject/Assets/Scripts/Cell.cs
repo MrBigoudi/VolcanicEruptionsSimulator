01f4d6797d7966388de2dff11ebf9b6dee4af2b7
MrBigoudi <yannis.kedadry@ens.psl.eu>
2023-04-11 14:27:59 +0200

    code refactoring + comments

--- unityProject/Assets/Scripts/Cell.cs
+++ unityProject/Assets/Scripts/Cell.cs
@@ -2,15 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * A class to manipulate cells for neighbour search
+*/
 public class Cell {
 
+    /**
+     * The list of particles inside the cell
+    */
     public ArrayList mParticles = new ArrayList();
 
+    /**
+     * The x coordinate of the cell
+    */
     public int mX;
+
+    /**
+     * The y coordinate of the cell
+    */
     public int mY;
+
+    /**
+     * The heigth of the cell
+    */
     public int mHeight;
+
+    /**
+     * The width of the cell
+    */
     public int mWidth;
 
+    /**
+     * A basic constructor
+    */
     public Cell(int x, int y, int height, int width){
         mX = x;
         mY = y;
--- unityProject/Assets/Scripts/Constants.cs
+++ unityProject/Assets/Scripts/Constants.cs
@@ -2,19 +2,49 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * A static class to use constants
+*/
 public static class Constants{
+    
+    /**
+     * The pi constant
+    */
     public const float PI = 3.1416f; 
 
-    // solver parameters
-    public const float G = 9.81f;          // gravitational force
-    public const float GAS_CONST = 2000.0f; // gas constant
+    /**
+     * The gravitational force
+    */
+    public const float G = 9.81f;
 
-    public const float H = 2.0f;           // kernel radius
+    /**
+     * The gas constant
+    */
+    public const float GAS_CONST = 2000.0f;
+
+    /**
+     * The kernel radius
+    */
+    public const float H = 2.0f;
+
+    /**
+     * Factor for poly6 kernel calculations
+    */
     public const float ALPHA_POLY6 = 4.0f / (PI*H*H*H*H*H*H*H*H);
     
-    public const float VISC = 200.0f;      // viscosity constant
-    public const float RHO_0 = 3500.0f;    // magma rest density
+    /**
+     * The viscosity constant
+    */
+    public const float VISC = 200.0f;
 
-    public const float STIFFNESS = G/RHO_0;   // stiffness constant for gas law 
+    /**
+     * The lava rest density
+    */
+    public const float RHO_0 = 3500.0f;
+
+    /**
+     * The stiffness constant for gas law
+    */
+    public const float STIFFNESS = G/RHO_0;
     
 }
