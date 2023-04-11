5eb8ac8be34813e34e96950a1389fe284fb5beaa
MrBigoudi <yannis.kedadry2@gmail.com>
2023-04-11 14:27:59 +0200

    code refactoring + comments

--- unityProject/Assets/Scripts/Particle.cs
+++ unityProject/Assets/Scripts/Particle.cs
@@ -2,34 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// Instantiate a rigidbody then set the velocity
-
+/**
+ * A class representing a particle
+*/
 public class Particle : MonoBehaviour{
 
     // public Vector3 mPosition = new Vector3();
+
+    /**
+     * The particle's velocity
+    */
     public Vector3 mVelocity = new Vector3();
 
+    /**
+     * The pressure force applied on the particle
+    */
     public Vector3 mPressureForce = new Vector3();
+
+    /**
+     * The acceleration force applied on the particle
+    */
     public Vector3 mAccelerationForce = new Vector3();
 
+    /**
+     * The particle's radius
+    */
     public static float mRadius = 0.35f; 
+
+    /**
+     * The particle's volume
+    */
     public static float mVolume = (4.0f/3.0f)*Constants.PI*mRadius*mRadius*mRadius;
+
+    /**
+     * The particle's mass
+    */
     public float mMass = mVolume * Constants.RHO_0; // volume * density
 
-    public float mRho = Constants.RHO_0; // density
-    public float mPressure = 0.0f; // presssure
+    /**
+     * The particle's density
+    */
+    public float mRho = Constants.RHO_0;
 
-    public Cell mCell = null; // to get the neighbours
-    public ArrayList mNeighbours = new ArrayList(); // to get the neighbours
+    /**
+     * The particle's pressure
+    */
+    public float mPressure = 0.0f;
 
+    /**
+     * The cell in which the particle's in
+    */
+    public Cell mCell = null;
+
+    /**
+     * The particle's neighbours
+    */
+    public ArrayList mNeighbours = new ArrayList();
+
+    /**
+     * Get the acceleration force applied on the particle
+     * @return The force as a vec3
+    */
     public Vector3 GetAcceleration(){
         return new Vector3(0.0f, -Constants.G, 0.0f);
     }
 
+    /**
+     * Assign a grid cell to the particle
+    */
     public void AssignGridCell(){
-        // redo this part
-        float pX = mPosition()[0];
-        float pY = mPosition()[1];
+        // TODO: redo this part
+        float pX = GetPosition()[0];
+        float pY = GetPosition()[1];
 
         int cX = (int)((pX+(Grid.mWidth/2.0f))/Grid.mCellWidth);
         int cY = (int)((pY+(Grid.mHeight/2.0f))/Grid.mCellHeight);
@@ -44,10 +88,19 @@ public class Particle : MonoBehaviour{
         Grid.mCells[cY,cX].mParticles.Add(this);
     }
 
-    public Vector3 mPosition(){
+    /**
+     * Get the particle's position
+     * @return The postion as a vec3
+    */
+    public Vector3 GetPosition(){
         return GetComponent<Rigidbody>().position;
     }
 
+    /**
+     * Update the rigid body of the particle
+     * @param newPos The Particle's new position
+     * @param newVel The Particle's new velocity
+    */
     public void UpdateRigidBody(Vector3 newPos, Vector3 newVel){
         // update internal position
         // mPosition = newPos;
