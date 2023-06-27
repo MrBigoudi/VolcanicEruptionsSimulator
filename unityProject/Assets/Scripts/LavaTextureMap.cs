0608142b56a6fcf68ea0dbd1b46100b7086554de
Yannis Kedadry <ykedadry@ad.inria.fr>
2023-06-27 13:25:36 +0200

    trying to generate an image

--- unityProject/Assets/Scripts/LavaTextureMap.cs
+++ unityProject/Assets/Scripts/LavaTextureMap.cs
@@ -13,7 +13,14 @@ public class LavaTextureMap : MonoBehaviour{
 
     List<Particle> _Particles;
     Vector3[] _Positions;
-    float[] _Heights;
+
+    float  [] _Heights;
+    Vector3[] _HeightsGradients;
+    float  [] _HeightsLaplacians;
+
+    float  [] _TerrainHeights;
+    Vector3[] _TerrainGradients;
+    float  [] _TerrainLaplacians;
 
     public Material _Material;
     private const int _ArraySize = 1023;
@@ -22,14 +29,15 @@ public class LavaTextureMap : MonoBehaviour{
     private RenderTexture _RenderTexture;
 
     private void GetTexture(){
-        int res = Terrain.activeTerrain.terrainData.heightmapResolution;
+        int res = (Terrain.activeTerrain.terrainData.heightmapResolution / 4) * 4;
         _RenderTexture = RenderTexture.GetTemporary(res, res);
         Graphics.Blit(null, _RenderTexture, _Material);
 
         //transfer image from rendertexture to texture
-        Texture2D texture = new Texture2D(res, res);
+        Texture2D texture = new Texture2D(res, res, TextureFormat.ARGB32, false);
         RenderTexture.active = _RenderTexture;
         texture.ReadPixels(new Rect(Vector2.zero, new Vector2(res, res)), 0, 0);
+        texture.Apply();  // Apply changes to the texture
 
         //save texture to file
         byte[] png = texture.EncodeToPNG();
@@ -63,15 +71,26 @@ public class LavaTextureMap : MonoBehaviour{
 
         // Generate indices for the points
         int[] indices = new int[_ArraySize];
-        Vector2[] uvs = new Vector2[_ArraySize];
+        Vector2[] heights = new Vector2[_ArraySize];
+        Vector3[] heightsGradients = new Vector3[_ArraySize];
+        Vector2[] terrain = new Vector2[_ArraySize];
+        Vector3[] terrainGradients = new Vector3[_ArraySize];
 
         for (int i = 0; i < _ArraySize; i++){
             indices[i] = i;
-            uvs[i] = new Vector2(_Heights[i], 0.0f);
+            heights[i] = new Vector3(_Heights[i], _HeightsLaplacians[i]);
+            heightsGradients[i] = _HeightsGradients[i];
+            terrain[i] = new Vector3(_TerrainHeights[i], _TerrainLaplacians[i]);
+            terrainGradients[i] = _TerrainGradients[i];
         }
         
         _Mesh.SetIndices(indices, MeshTopology.Points, 0);
-        _Mesh.SetUVs(0, uvs);
+        _Mesh.SetUVs(0, heights);
+        _Mesh.SetUVs(1, terrain);
+        _Mesh.SetUVs(2, heightsGradients);
+        _Mesh.SetUVs(3, terrainGradients);
+
+        _Material.SetFloat("dt", Time.deltaTime);
 
         _Mesh.UploadMeshData(false);
     }
@@ -80,26 +99,34 @@ public class LavaTextureMap : MonoBehaviour{
      * Update the lava's heights
     */
     public void Updt(List<Particle> particles){
-        FetchPositions(particles);
-        FetchHeights(particles);
+        FetchParticlesInfos(particles);
         UpdtMesh();
         GetTexture();
     }
 
-    private void FetchPositions(List<Particle> particles){
+    private void FetchParticlesInfos(List<Particle> particles){
         _Positions = new Vector3[_ArraySize];
-        for(int i=0; i<particles.Count; i++){
-            Particle p = particles[i];
-            _Positions[i] = p.GetPosition();
-        }
-    }
 
-    private void FetchHeights(List<Particle> particles){
         _Heights = new float[_ArraySize];
+        _HeightsGradients = new Vector3[_ArraySize];
+        _HeightsLaplacians = new float[_ArraySize];
+
+        _TerrainHeights = new float[_ArraySize];
+        _TerrainGradients = new Vector3[_ArraySize];
+        _TerrainLaplacians = new float[_ArraySize];
+
         for(int i=0; i<particles.Count; i++){
             Particle p = particles[i];
+
+            _Positions[i] = p.GetPosition();
+
             _Heights[i] = p.mHeight;
+            _HeightsGradients[i] = p.mHeightGradient;
+            _HeightsLaplacians[i] = p.mHeightLaplacian;
+
+            _TerrainHeights[i] = StaggeredGridV2.GetHeight(p.GetPosition());
+            _TerrainGradients[i] = StaggeredGridV2.GetGradient(p);
+            _TerrainLaplacians[i] = StaggeredGridV2.GetLaplacian(p.GetPosition());
         }
     }
-
 }
